cd1436d0d9da3229c6fb8ad9fc68cd28daaa52b5
varshadhasaraju-9 <varshadhasaraju9@gmail.com>
2026-01-04 18:53:07 +0530

    Add files via upload

---
+++ Assign1_Task_1.py
@@ -0,0 +1,18 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+#addition
+add = num1 + num2
+print("Addition:", add)
+
+#subtraction
+sub = num1 - num2
+print("Subtraction", sub)
+
+#multiplication
+mul = num1 * num2
+print("Multiplication", mul)
+
+#division
+divi = num1 / num2
+print("Division:", round(divi, 2))
